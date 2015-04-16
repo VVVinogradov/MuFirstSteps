bdaed426c6becf972918808e2e0ababa746fdeb7
VVVinogradov <VVVinogradov@outlook.com>
2015-04-16 16:58:36 +0300

    t5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.lang.System;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Test");
     }
 }
